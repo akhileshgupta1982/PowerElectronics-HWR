46101e9f66489a211dcf28d0bd7d62d9efd77a15
SakshiAgarwal7 <67471915+SakshiAgarwal7@users.noreply.github.com>
2020-08-24 11:48:29 +0530

    Add files via upload

---
+++ experiment/Simulator/fr_change.js
@@ -0,0 +1,10 @@
+function Frq(){
+   fr=document.getElementById("fo").value;
+    document.getElementById("Frequency").value=fr;
+	
+}
+function Vmax(){
+   V_range=document.getElementById("Vmax").value;
+    document.getElementById("V_max").value= V_range;
+	
+}
