a2dd92f13f12d8bfd72816d92bb9bfca426accdf
SakshiAgarwal7 <67471915+SakshiAgarwal7@users.noreply.github.com>
2020-10-24 15:08:53 +0530

    Add files via upload

---
+++ experiment/Simulator/javascript/RL_Vm_change.js
@@ -0,0 +1,15 @@
+function Vchange(){
+   Vm=document.getElementById("Vp").value;
+document.getElementById("Voltage_amplitude").value=Vm;
+
+}
+function Rchange(){
+   R=document.getElementById("RL").value;
+document.getElementById("Load_Resistance").value=R;
+
+}
+function Lchange(){
+   L1=document.getElementById("L").value;
+document.getElementById("Load_Inductance").value=L1;
+
+}
